9da98ccd105a09de59d920bfce23967d6a89369d
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-11-20 11:40:21 +0000

    Implementing a search function

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,6 +3,7 @@ import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
 import moment from 'moment'
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 import Loader from './Loader'
 
 const demoImage =
@@ -17,10 +18,29 @@ const News = ({ simplified }) => {
     newsCategory,
     count: simplified ? 6 : 12,
   })
+  const { data } = useGetCryptosQuery(100)
 
   if (!cryptoNews?.value) return <Loader />
   return (
     <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }>
+            <Option value="Cryptocurrency"></Option>
+            {data?.data?.coins.map((coin) => (
+              <Option value={coin.name}>{coin.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      )}
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
           <Card hoverable className="news-card">
